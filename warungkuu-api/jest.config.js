2a32a2e1d5bbdba91e3be447d5bdccbbfc3c9c4f
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-04-17 16:05:08 +0700

    integrating with sonarcloud

--- warungkuu-api/jest.config.js
+++ warungkuu-api/jest.config.js
@@ -7,6 +7,20 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   clearMocks: true,
+  reporters: [
+    "default",
+    [
+      "jest-sonar-reporter",
+      {
+        outputDirectory: "coverage",
+        outputName: "test-report.xml"
+      }
+    ]
+  ],
+
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text"],
   moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
   transform: {
     ...tsJestTransformCfg,
